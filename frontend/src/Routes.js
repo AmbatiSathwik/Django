0fad175341d98c9784f1a511dcf93d2d5946981d
AmbatiSathwik <ambatisathwil2311@gmail.com>
2021-10-22 01:53:28 -0400

    auth index done

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './core/Home';
 import Base from './core/Base';
+import PrivateRoutes from './auth/helper/PrivateRoutes';
+
 
 const Routes = () => {
     return(
         <Router>
             <Switch>
                 <Route path="/" component={Home} exact />
-                <Route path="/base/" component={Base} exact />
+                {/*<PrivateRoutes path="/user/dashboard/" exact component={} />*/}
             </Switch>
         </Router>
     );
--- frontend/src/auth/helper/PrivateRoutes.js
+++ frontend/src/auth/helper/PrivateRoutes.js
@@ -1,1 +1,26 @@
-//
+import React, { Component } from 'react';
+import { Route, Redirect } from 'react-route-dom';
+import {isAutenticated} from './index';
+
+function PrivateRoutes({ children, ...rest }) {
+    
+    return (
+      <Route
+        {...rest}
+        render={(props) =>
+        isAutenticated ? (
+            <Component {...props} />
+          ) : (
+            <Redirect
+              to={{
+                pathname: "/signin",
+                state: { from: props.location }
+              }}
+            />
+          )
+        }
+      />
+    );
+}
+
+export default PrivateRoutes
